a0085a8baac958d2648ce4e87fdd6cd64053d675
greenhornsr <srollins11279@gmail.com>
2019-05-29 12:21:14 -0700

    initial commit.  set up server.listen.  corrected syntax in server.js to properly require express

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code away!
+// const express = require('express')
+const server = require('./server')
+
+
+
+server.listen(4000, () => console.log('\n***Server is listening on http://localhost:4000***\n'))
