32963f433657bc740af6f84d65faf35c4b03dd2d
greenhornsr <srollins11279@gmail.com>
2019-05-29 14:04:39 -0700

    added validateById middleware function.  built out core get route for all users and getbyid route.  confirmed working

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,21 +1,50 @@
-const express = 'express';
+const express = require('express');
 
 const router = express.Router();
+router.use(express.json());
+
+const db = require('./userDb');
+
+
 
 router.post('/', (req, res) => {
-
+    const newuser = req.user;
+    db.insert(newuser)
+    .then(res => {
+        const allusers = db.get()
+        res.status(200).json({ success: true, message: `${newuser} added successfully!`}, allusers)
+    })
+    .catch(err => {
+        res.status(500).json({ success: false, message: 'bummer...', err })
+    })
 });
 
 router.post('/:id/posts', (req, res) => {
 
 });
 
+//calling find returns a promise that resolves to an array of all the resources contained in the database.
 router.get('/', (req, res) => {
-
+    db.get()
+    .then(users => {
+        res.status(200).json({ success: true, message: 'Users located!', users })
+    })
+    .catch(err => {
+        res.status(400).json({ success: false, err, message: 'No users here mate!' })
+    })
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    // console.log(req.user)
+    // console.log(res)
+    const { id } = req.user
+    db.getById(id)
+    .then(user => {
+        res.status(200).json({ success: true, message: `${user.name} located!`, user })
+    })
+    .catch(err => {
+        res.status(400).json({ success: false, err, message: 'No users here mate!' })
+    })
 });
 
 router.get('/:id/posts', (req, res) => {
@@ -33,8 +62,14 @@ router.put('/:id', (req, res) => {
 //custom middleware
 
 function validateUserId(req, res, next) {
-
-};
+    if(req.params && req.params.id) {
+        // console.log(req.params)
+        req.user = req.params;
+        next();
+    }else {
+        res.status(400).json({ message: 'Invalid user id!' });
+    }
+    } 
 
 function validateUser(req, res, next) {
 
